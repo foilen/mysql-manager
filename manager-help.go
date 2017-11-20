921f6fc9c022c590a005ba05762a7214584a8324
Simon Levesque <simon@simonlevesque.com>
2017-11-19 20:09:21 -0500

    Fix help

--- manager-help.go
+++ manager-help.go
@@ -7,8 +7,5 @@ import (
 func showHelp() {
 	fmt.Println("Arguments:")
 	fmt.Println("  hostname:port : The host and port to connect to")
-	fmt.Println("  admin username : Most likely 'root'")
-	fmt.Println("  admin password file : The file with the password")
-	fmt.Println("  databases.txt : The list of database names to keep or create")
-	fmt.Println("  userPermissions.json : The list of users with their database access")
+	fmt.Println("  manager-config.json : The admin credentials, the databases and the users permissions")
 }
